a91d537e650fc890c95bb75b280dfeb65f348e0a
sabastain wakoyi <jevian@WAKOYIs-MacBook-Pro.local>
2021-10-15 14:33:33 -0500

    exercise 10 done

--- code.js
+++ code.js
@@ -2,3 +2,11 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+function sayHello() {
+    return typeof 'Hello Jevi'
+}
+
+function isFive(n) {
+    return n == 5;
+}
--- tests.js
+++ tests.js
@@ -13,3 +13,44 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+
+// write a test for a function named 'sayHello'
+
+describe('sayHello', function() {
+    it('should be define as a function', function() {
+        expect(typeof sayHello).toBe('function');
+    })
+
+    it('should return a string', function() {
+        expect(typeof sayHello()).toBe('string');
+    })
+    it('should return a greeting for the input', function() {
+        expect(sayHello('Jevi')).toBe('string');
+    })
+})
+
+// write a test for a function named 'isFive'
+describe('isFive', function() {
+    it('should be defined as a function', function() {
+        expect(typeof isFive).toBe('function');
+    })
+    it('should return a boolean', function() {
+        expect(typeof isFive(5)).toBe('boolean');
+        expect(typeof isFive(1)).toBe('boolean');
+    })
+    it('should return true for the input', function() {
+        expect(isFive('5')).toBe(true);
+    })
+    it('should return true when passes 5', function () {
+    expect(isFive('5')).toBe(true);
+    })
+
+
+})
+
+
+//isEven
+describe('isEven',function() {
+    it('')
+})
